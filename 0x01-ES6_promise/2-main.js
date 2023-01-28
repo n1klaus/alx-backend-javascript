f7a2bbdb08c52245b73d3fa13f077252174cf2e9
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 14:37:45 +0000

    Update 2-then.js

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,4 +1,9 @@
 import handleResponseFromAPI from './2-then';
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+const promise1 = Promise.resolve();
+const response1 = handleResponseFromAPI(promise1);
+console.log(response1);
+
+const promise2 = Promise.reject(new Error());
+const response2 = handleResponseFromAPI(promise2);
+console.log(response2);
