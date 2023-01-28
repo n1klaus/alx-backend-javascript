db2581049ce35e962ce1800bb553d7125bbf254b
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 12:19:11 +0000

    Add 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-  const report = new Object({
+  const report = {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
-  });
+  };
   return report;
 }
