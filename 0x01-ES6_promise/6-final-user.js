3a787d0406599350743bdfd5c0fc9ab9edd97138
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 15:28:41 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-	const user = signUpUser(firstName, lastName);
-	const photo = uploadPhoto(fileName);
-	Promise.all([user, photo])
-	.then((data) => {
-		const result = Object.assign({}, ...data);
-		return Promise.resolve(new Array({"status": "success", "value": result}));
-	})
-	.catch((error) => {
-		return Promise.reject(new Array({"status": "failed", "value": error}));
-	});
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+  Promise.all([user, photo])
+    .then((data) => {
+      const result = Object.assign({}, ...data);
+      return Promise.resolve(new Array({ status: 'success', value: result }));
+    })
+    .catch((error) => Promise.reject(new Array({ status: 'failed', value: error })));
 }
