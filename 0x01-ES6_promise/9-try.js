95d8f783677d8ff7bd644048f8651c8bac839513
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 21:47:59 +0000

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,9 +1,17 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  const calc = Promise.resolve(mathFunction());
-  calc
-    .then((value) => { queue.push(value); })
-    .catch((error) => { queue.push(error); })
+  const calc = Promise.resolve(mathFunction);
+  return calc
+    .then(
+      (value) => {
+        queue.push(value);
+        return value;
+      },
+      (error) => {
+        queue.push(error);
+        return error;
+      },
+    )
     .then(() => {
       queue.push('Guardrail was processed');
       return queue;
