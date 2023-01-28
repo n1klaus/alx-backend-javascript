b96250ab5b1a56db9740e7765460792db393a93a
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 13:31:02 +0000

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   if (success) {
-	  return Promise.resolve(new Object(
-		  {
-			  "status": 200,
-			  "body": "Success"
-		  }
-	  ));
-	 }
-	const error = new Error("The fake API is not working currently");
-	return Promise.reject(error);
+    return Promise.resolve(
+      {
+        status: 200,
+        body: 'Success',
+      },
+    );
+  }
+  const error = new Error('The fake API is not working currently');
+  return Promise.reject(error);
 }
