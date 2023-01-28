901e2fd833784c9eb5989398387a4988d09d2d18
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 21:19:07 +0000

    Update 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  Promise.race([chinaDownload, USDownload])
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+  return Promise.race([chinaDownload, USDownload])
+    .then((data) => data);
 }
