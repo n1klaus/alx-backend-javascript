f268ae2d5b2856ba8e498fe94fb5030d366291c2
Nick Nyanjui <nicknyanjui@gmail.com>
2023-01-28 15:00:14 +0000

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './util';
 export default function handleProfileSignup() {
   const upload = uploadPhoto();
   const user = createUser();
-  Promise.all([upload, user])
+  const resp = Promise.all([upload, user])
     .then((data) => {
       const item = Object.assign({}, ...data);
       const result = `${item.body} ${item.firstName} ${item.lastName}`;
@@ -14,4 +14,5 @@ export default function handleProfileSignup() {
       console.error('Signup system offline');
       return error;
     });
+  return resp;
 }
