6fe54dae6eb0ca423944b98fa8cdf3e03114103d
Nick Nyanjui <nicknyanjui@gmail.com>
2023-02-04 20:19:04 +0000

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,16 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  if (buffer.byteLength !== length) {
-    throw new Error('Position outside range');
+  try {
+    const buffer = new ArrayBuffer(length);
+    if (buffer.byteLength !== length) {
+      throw new Error();
+    }
+    const view = new DataView(buffer);
+    view.setInt8(position, value);
+    return view;
+  } catch (e) {
+    if (e instanceof RangeError) {
+      throw new Error('Position outside range');
+    }
   }
-  const view = new DataView(buffer);
-  view.setInt8(position, value);
-  return view;
+  return null;
 }
