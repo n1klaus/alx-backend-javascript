8389dc4ee34bca55f2aa104bc76b8de63b3898c9
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-12 14:04:45 +0000

    Add 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building
+      && !('evacuationWarningMessage' in this)) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
