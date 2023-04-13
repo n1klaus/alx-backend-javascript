dd0d1897a7f2d9c4192d8366f65648a5431ee1c4
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-13 15:19:26 +0000

    Update 10-loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const result = [];
   for (const idx of array) {
-    array[array.indexOf(idx)] = appendString + idx;
+    result.push(appendString + idx);
   }
-
-  return array;
+  return result;
 }
