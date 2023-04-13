10d80fc4be3be493693a41d87ab01c535882b9ca
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-13 17:52:32 +0000

    Update 100-await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-	const result = {"photo": null, "user": null};
-	try {
-		const user = await createUser();
-		const photo = await uploadPhoto();
-		result = {"photo": photo, "user": user};
-	}
-	finally {
-		return result;
-	}
+  const result = { photo: null, user: null };
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    result.photo = photo;
+    result.user = user;
+    return result;
+  } catch (err) {
+    return result;
+  }
 }
