0d015eec5cfdfae521ae11b356d0b711ac95ae8b
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-13 18:19:11 +0000

    Update 8-clean_set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const result = [];
   if (typeof startString === 'string' && startString !== '') {
     set.forEach((elem) => {
-      if (elem.startsWith(startString)) {
+      if (typeof elem === 'string' && elem.startsWith(startString)) {
         result.push(elem.slice(startString.length));
       }
     });
