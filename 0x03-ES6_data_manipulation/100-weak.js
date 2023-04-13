b30500f19db190db419b7a7961e5106aacc1dcba
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-13 19:21:54 +0000

    Update 100-weak

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,12 +5,11 @@ const queryAPI = (endpoint) => {
   if (weakMap.has(endpoint)) {
     value = weakMap.get(endpoint);
   }
-  if (value >= 5) {
+  if (value >= 4) {
     throw new Error('Endpoint load is high');
   }
   value += 1;
   weakMap.set(endpoint, value);
-  console.log(value);
 };
 
 export { weakMap, queryAPI };
