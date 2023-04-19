236678d1d3fbf4d4ef5409b8456d7076a75cec68
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 21:48:15 +0000

    Update 9-api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,19 +1,18 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
+const app = express();
 
+app.get('/', (req, resp) => {
+  resp.status(200).send('Welcome to the payment system');
+});
 
-app.get("/", (req, resp) => {
-    resp.status(200).send("Welcome to the payment system")
-})
-
-app.get("/cart/:id", (req, resp) => {
-    if (isNaN(req.params.id)) {
-        return resp.status(404)
-    }
-    resp.status(200).send(`Payment methods for cart ${req.params.id}`)
-})
+app.get('/cart/:id', (req, resp) => {
+  if (isNaN(req.params.id)) {
+    return resp.status(404);
+  }
+  resp.status(200).send(`Payment methods for cart ${req.params.id}`);
+});
 
 app.listen(7865, () => {
-    console.log("API available on localhost port 7865")
-})
+  console.log('API available on localhost port 7865');
+});
