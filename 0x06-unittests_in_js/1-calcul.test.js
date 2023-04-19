a57191cede05096f4e9d9ce8ea601e02615fb04e
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 21:18:03 +0000

    Update 1-calcul

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./1-calcul');
 const { describe, it } = require('mocha');
 
 describe('calculateNumber with type SUM', function () {
