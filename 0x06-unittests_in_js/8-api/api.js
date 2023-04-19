9f7453155827f7a0ea41c4a8837913d7d9d14f9e
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 21:47:03 +0000

    Update 8-api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,12 +1,11 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
+const app = express();
 
-
-app.get("/", (req, resp) => {
-    resp.status(200).send("Welcome to the payment system")
-})
+app.get('/', (req, resp) => {
+  resp.status(200).send('Welcome to the payment system');
+});
 
 app.listen(7865, () => {
-    console.log("API available on localhost port 7865")
-})
+  console.log('API available on localhost port 7865');
+});
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,29 +1,29 @@
-const { expect } = require("chai")
-const {suite, setup, teardown, test} = require("mocha")
-const {fakeServer, spy} = require("sinon")
-const request = require("request")
+const { expect } = require('chai');
+const { suite, setup, teardown, test } = require('mocha');
+const { fakeServer, spy } = require('sinon');
+const request = require('request');
 
-suite("Index page", function() {
-    setup(function () {
-        this.server = fakeServer.create()
-    })
-    teardown(function() {
-        this.server.restore()
-    })
-    test("tests the index endpoint", function(done) {
-        this.server.respondWith("GET", "/", [
-                200,
-                { "Content-Type": "application/json" },
-                "Welcome to the payment system"
-            ]
-        )
-        const spiedRequest = spy(request, "get")
-        const result = spiedRequest("http://localhost:7865/")
-        done()
-        this.server.respond()
-        expect(spiedRequest.calledOnceWith("http://localhost:7865/")).to.be.true
-        expect(result.data).to.equal("Welcome to the payment system")
-        expect(result.status).to.equal(200)
-        // expect(result.contentType).to.equal({ "Content-Type": "application/json" })
-    })
-})
+suite('Index page', function () {
+  setup(function () {
+    this.server = fakeServer.create();
+  });
+  teardown(function () {
+    this.server.restore();
+  });
+  test('tests the index endpoint', function (done) {
+    this.server.respondWith('GET', '/', [
+      200,
+      { 'Content-Type': 'application/json' },
+      'Welcome to the payment system'
+    ]
+    );
+    const spiedRequest = spy(request, 'get');
+    const result = spiedRequest('http://localhost:7865/');
+    done();
+    this.server.respond();
+    expect(spiedRequest.calledOnceWith('http://localhost:7865/')).to.be.true;
+    expect(result.data).to.equal('Welcome to the payment system');
+    expect(result.status).to.equal(200);
+    // expect(result.contentType).to.equal({ "Content-Type": "application/json" })
+  });
+});
