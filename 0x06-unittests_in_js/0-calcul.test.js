1b748c44349ab9f6e42c8376691fc098994fc6e5
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 20:19:47 +0000

    Add 0-calcul.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,22 @@
+import assert from 'assert';
+import calculateNumber from './0-calcul';
+import { describe, it } from 'mocha';
+
+describe('calculateNumber', function () {
+  it('should add positive integer arguments', function () {
+    const result = calculateNumber(5, 2);
+    assert.equal(7, result, 'success');
+  });
+  it('should add negative integer arguments', function () {
+    const result = calculateNumber(-5, -2);
+    assert.equal(-7, result, 'success');
+  });
+  it('should add positive float arguments', function () {
+    const result = calculateNumber(1.6, 2.1);
+    assert.equal(4, result, 'success');
+  });
+  it('should add negative float arguments', function () {
+    const result = calculateNumber(-1.9, 2.1);
+    assert.equal(0, result, 'success');
+  });
+});
