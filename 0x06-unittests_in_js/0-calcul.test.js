2bfd356a1ae0edc36478eecaa56c9f916596f98d
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 21:03:53 +0000

    Update 0-calcul

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,15 +4,15 @@ const { describe, it } = require('mocha');
 
 describe('calculateNumber', function () {
   it('should round the first argument', function () {
-    const result = calculateNumber(5.2, 2);
-    assert.strictEqual(7, result, 'success');
+    const result = calculateNumber(5.9, 2);
+    assert.strictEqual(8, result, 'success');
   });
   it('should round the second argument', function () {
     const result = calculateNumber(5, 2.6);
     assert.strictEqual(8, result, 'success');
   });
   it('should round both argument', function () {
-    const result = calculateNumber(1.6, 2.1);
-    assert.strictEqual(4, result, 'success');
+    const result = calculateNumber(1.6, 2.8);
+    assert.strictEqual(5, result, 'success');
   });
 });
