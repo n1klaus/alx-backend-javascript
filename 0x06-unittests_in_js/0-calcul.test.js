ca3f0a1bd9d0ae65a3c88f13a0c3c5558c3abab4
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 20:38:13 +0000

    Update 0-calcul

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,20 +3,16 @@ const calculateNumber = require('./0-calcul');
 const { describe, it } = require('mocha');
 
 describe('calculateNumber', function () {
-  it('should add positive integer arguments', function () {
-    const result = calculateNumber(5, 2);
+  it('should round the first argument', function () {
+    const result = calculateNumber(5.2, 2);
     assert.equal(7, result, 'success');
   });
-  it('should add negative integer arguments', function () {
-    const result = calculateNumber(-5, -2);
-    assert.equal(-7, result, 'success');
+  it('should round the second argument', function () {
+    const result = calculateNumber(5, 2.6);
+    assert.equal(8, result, 'success');
   });
-  it('should add positive float arguments', function () {
+  it('should round both argument', function () {
     const result = calculateNumber(1.6, 2.1);
     assert.equal(4, result, 'success');
-  });
-  it('should add negative float arguments', function () {
-    const result = calculateNumber(-1.9, 2.1);
-    assert.equal(0, result, 'success');
   });
 });
