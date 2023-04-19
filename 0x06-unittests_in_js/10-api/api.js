e60d83da19901246b1796f00854d6db23a16147f
Nick Nyanjui <nicknyanjui@gmail.com>
2023-04-19 21:55:46 +0000

    Update 10-api

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,31 +1,32 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
+const app = express();
 
+app.get('/', (req, resp) => {
+  resp.status(200).send('Welcome to the payment system');
+});
 
-app.get("/", (req, resp) => {
-    resp.status(200).send("Welcome to the payment system")
-})
+app.get('/cart/:id', (req, resp) => {
+  if (isNaN(req.params.id)) {
+    return resp.status(404);
+  }
+  resp.status(200).send(`Payment methods for cart ${req.params.id}`);
+});
 
-app.get("/cart/:id", (req, resp) => {
-    if (isNaN(req.params.id)) {
-        return resp.status(404)
+app.get('/available_payments', (req, resp) => {
+  const respObject = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
     }
-    resp.status(200).send(`Payment methods for cart ${req.params.id}`)
-})
+  };
+  resp.status(200).json(respObject.toString());
+});
 
-app.get("/available_payments", (req, resp) => {
-    const respObject = {payment_methods: {
-        credit_cards: true,
-        paypal: false
-      }}
-    resp.status(200).json(respObject.toString())
-})
-
-app.post("/login", (req, resp) => {
-    resp.status(200).send(`Welcome ${req.body.userName}`)
-})
+app.post('/login', (req, resp) => {
+  resp.status(200).send(`Welcome ${req.body.userName}`);
+});
 
 app.listen(7865, () => {
-    console.log("API available on localhost port 7865")
-})
+  console.log('API available on localhost port 7865');
+});
